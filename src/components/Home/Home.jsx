03f1c7d5a3c578557c23c680ff574bf0a252d2de
Coder And Creator <70572678+c4coderandcreator@users.noreply.github.com>
2024-05-21 22:21:25 +0530

    Create Landing

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,68 @@ const Home = () => {
   return (
     <>
       <Navbar />
-      <div className="font-[CamptonBold] text-[3rem]">Home Page</div>
+      <div className="flex p-20 mx-10 justify-between">
+        <div className="text-[1.25rem] mr-20 al">
+          <div className="flex">
+            <div>
+              <img
+                src="https://raw.githubusercontent.com/c4coderandcreator/ahyliz/42a9b7f29a07ac9f0abf60584a89e918ec8aa762/Resources/Home/Icons/like.svg"
+                alt="img"
+              />
+            </div>
+            <div className="mx-2 font-[CamptonMedium] pb-4">
+              <p>Welcome To Ahyliz Technologies</p>
+            </div>
+          </div>
+          <p className="text-[4.5rem] font-[CamptonBold] leading-[4.875rem]">
+            Leading the <br /> Way in Digital <br /> Innovation.
+          </p>
+          <p className="py-2">Empowering Your Digital Future</p>
+          <div className="flex items-center mt-8 font-[CamptonBook]">
+            <p className="px-7 py-3 bg-[#4f45e2] rounded-lg text-white">
+              Explore more
+            </p>
+            <p className="ml-14 text-[#4f45e2]">contact us</p>
+          </div>
+
+          <div className="flex text-[#4f45e2] text-[1rem] py-6">
+            <div>
+              <p className="tracking-[0.3rem] font-[CamptonSemiBold]">
+                FOLLOW US
+              </p>
+            </div>
+            <div className="flex">
+              <img
+                className="pl-6 pr-2"
+                src="https://raw.githubusercontent.com/c4coderandcreator/ahyliz/42a9b7f29a07ac9f0abf60584a89e918ec8aa762/Resources/Home/Icons/Group%20171.svg"
+                alt="insta"
+              />
+              <img
+                className="px-2"
+                src="https://raw.githubusercontent.com/c4coderandcreator/ahyliz/42a9b7f29a07ac9f0abf60584a89e918ec8aa762/Resources/Home/Icons/Group%20170.svg"
+                alt="linkedin"
+              />
+              <img
+                className="px-2"
+                src="https://raw.githubusercontent.com/c4coderandcreator/ahyliz/42a9b7f29a07ac9f0abf60584a89e918ec8aa762/Resources/Home/Icons/Path%208.svg"
+                alt="x.com"
+              />
+              <img
+                className="px-2"
+                src="https://raw.githubusercontent.com/c4coderandcreator/ahyliz/42a9b7f29a07ac9f0abf60584a89e918ec8aa762/Resources/Home/Icons/facebook.svg"
+                alt="facebook"
+              />
+            </div>
+          </div>
+        </div>
+        <div>
+          <img
+            className="h-[29.375rem] w-[38.187rem] ml-20"
+            src="https://raw.githubusercontent.com/c4coderandcreator/ahyliz/42a9b7f29a07ac9f0abf60584a89e918ec8aa762/Resources/Home/Images/Illustration.svg"
+            alt="img"
+          />
+        </div>
+      </div>
     </>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,18 +2,24 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="flex mx-10">
-      <div className="px-4">
-        <img src="" alt="" />
+    <div className="flex mx-20 justify-between h-[6.25rem]">
+      <div className="px-6 py-4">
+        <img
+          src="https://raw.githubusercontent.com/c4coderandcreator/ahyliz/42a9b7f29a07ac9f0abf60584a89e918ec8aa762/Resources/Home/Images/Logo.svg"
+          alt="logo"
+        />
       </div>
-      <div>
-        <p className="px-6 py-2">Home</p>
-        <p className="px-6 py-2">About</p>
-        <p className="px-6 py-2">Services</p>
-        <p className="px-6 py-2">Careers</p>
-        <p className="px-6 py-2">Contact Us</p>
-        <p className="px-6 py-2">Legal</p>
-        <p className="px-6 py-2"> Get Started</p>
+      <div className="flex items-center font-[CamptonBook]">
+        <p className="px-7 py-2 text-[1.25rem]">Home</p>
+        <p className="px-7 py-2 text-[1.25rem]">About</p>
+        <p className="px-7 py-2 text-[1.25rem]">Services</p>
+        <p className="px-7 py-2 text-[1.25rem]">Careers</p>
+        <p className="px-7 py-2 text-[1.25rem]">Contact Us</p>
+        <p className="px-7 py-2 text-[1.25rem]">Legal</p>
+        <p className="px-7 py-4 mx-6 bg-[#4f45E2] rounded-lg text-[#ffffff]">
+          {" "}
+          Get Started
+        </p>
       </div>
     </div>
   );
