42a9b7f29a07ac9f0abf60584a89e918ec8aa762
Coder And Creator <70572678+c4coderandcreator@users.noreply.github.com>
2024-05-21 19:56:14 +0530

    initial commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex mx-10">
+      <div className="px-4">
+        <img src="" alt="" />
+      </div>
+      <div>
+        <p className="px-6 py-2">Home</p>
+        <p className="px-6 py-2">About</p>
+        <p className="px-6 py-2">Services</p>
+        <p className="px-6 py-2">Careers</p>
+        <p className="px-6 py-2">Contact Us</p>
+        <p className="px-6 py-2">Legal</p>
+        <p className="px-6 py-2"> Get Started</p>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
