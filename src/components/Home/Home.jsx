1990141ac726ac865eb2c2f087393635ca57608c
Coder And Creator <70572678+c4coderandcreator@users.noreply.github.com>
2024-05-21 17:28:52 +0530

    initial commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="font-[CamptonBold] text-[3rem]">Home Page</div>
+    </>
+  );
+};
+
+export default Home;
